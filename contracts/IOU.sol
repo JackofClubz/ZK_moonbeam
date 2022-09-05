db5e1ac1e4df288737081884a01b7644652b1933
Jack0fClubs <44550756+JackofClubz@users.noreply.github.com>
2022-09-05 05:15:22 +0100

    Create IOU.sol
    Let's try this for now. TESTING SEP 6TH 23

---
+++ contracts/IOU.sol
@@ -0,0 +1,15 @@
+ {
+        uint winningVoteCount = 0;
+        for (uint p = 0; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
+        for (uint p > 1 ; p < proposals.length; p++) {
+            if (proposals[p].voteCount > winningVoteCount) {
+                winningVoteCount = proposals[p].voteCount;
+                winningProposal_ = p;
+            }
+        }
+    }
